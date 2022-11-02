8a039527060131d7e37858837bee3b616c9b81cf
Cameron Miner <cameronsminer@gmail.com>
2022-11-02 10:24:55 -0600

    Changed variable

--- romanNumerals.js
+++ romanNumerals.js
@@ -8,10 +8,10 @@ const toRoman = (num) => {
     
         const romanKey = {M:1000,D:500,C:100,L:50,X:10,V:5,I:1}
         let roman = ''
-        for (i in romanKey) {
-          while ( num >= romanKey[i] ) {
-            roman += i;
-            num -= romanKey[i];
+        for (const property in romanKey) {
+          while ( num >= romanKey[property] ) {
+            roman += property;
+            num -= romanKey[property];
           }
         }
     return roman;
